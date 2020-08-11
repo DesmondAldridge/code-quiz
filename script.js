41c4bb6dcd032caf49b2d29bae47908b4bd681d6
Desmond <desmondaldridge@gmail.com>
2020-08-11 15:19:51 -0700

    Structured all the questions and declared a few other variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Global variables
+// GLOBAL VARIABLES
 
 var theTimer = document.getElementById("timer");
 var theTime = document.getElementById("time");
@@ -21,6 +21,67 @@ var fourthChoice = document.getElementById("choice4");
 //
 //
 
+// QUESTIONS
+
+function beginQuiz() {
+  //First question...
+  questions.textcontent = "How many ___ are in ___?";
+  firstChoice.textcontent = "a__";
+  secondChoice.textcontent = "b__";
+  thirdChoice.textcontent = "c__";
+  fourthChoice.textcontent = "d__";
+  //Make them choosable
+    firstChoice.addEventListener('click', function(){alert('Incorrect')});
+    secondChoice.addEventListener('click', function(){alert('Incorrect')});
+    thirdChoice.addEventListener('click', function(){alert('Correct!'), contQuiz()});
+    fourthChoice.addEventListener('click', function(){alert('Incorrect')});
+}
+
+function contQuiz () {
+  //Second question...
+  questions.textcontent = "which ___ are ___?";
+  firstChoice.textcontent = "a__";
+  secondChoice.textcontent = "b__";
+  thirdChoice.textcontent = "c__";
+  fourthChoice.textcontent = "d__";
+  //Make them choosable
+    firstChoice.addEventListener('click', function(){alert('Incorrect')});
+    secondChoice.addEventListener('click', function(){alert('Correct!'), contQuiz2()});
+    thirdChoice.addEventListener('click', function(){alert('Incorrect')});
+    fourthChoice.addEventListener('click', function(){alert('Incorrect')});
+}
+
+function contQuiz2 () {
+  //Second question...
+  questions.textcontent = "How is ___?";
+  firstChoice.textcontent = "a__";
+  secondChoice.textcontent = "b__";
+  thirdChoice.textcontent = "c__";
+  fourthChoice.textcontent = "d__";
+  //Make them choosable
+    firstChoice.addEventListener('click', function(){alert('Incorrect')});
+    secondChoice.addEventListener('click', function(){alert('Incorrect')});
+    thirdChoice.addEventListener('click', function(){alert('Incorrect')});
+    fourthChoice.addEventListener('click', function(){alert('Correct!'), contQuiz3()});
+}
+
+function contQuiz3 () {
+  //Second question...
+  questions.textcontent = "____?";
+  firstChoice.textcontent = "a__";
+  secondChoice.textcontent = "b__";
+  thirdChoice.textcontent = "c__";
+  fourthChoice.textcontent = "d__";
+  //Make them choosable
+    firstChoice.addEventListener('click', function(){alert('Incorrect')});
+    secondChoice.addEventListener('click', function(){alert('Incorrect')});
+    thirdChoice.addEventListener('click', function(){alert('Incorrect')});
+    fourthChoice.addEventListener('click', function(){alert('Correct!'), endPage()});
+}
+
+
+
+
 // ONTO THE QUIZ...
 
 //
@@ -29,11 +90,21 @@ var fourthChoice = document.getElementById("choice4");
 var secondsLeft = 60;
 
 function beginQuiz() {
-  
+  //First question...
+  questions.textcontent = "How many ___ are in ___?";
+  firstChoice.textcontent = "a__";
+  secondChoice.textcontent = "b__";
+  thirdChoice.textcontent = "c__";
+  fourthChoice.textcontent = "d__";
+  //Make them choosable
+    firstChoice.addEventListener('click', function(){alert('Incorrect')});
+    secondChoice.addEventListener('click', function(){alert('Incorrect')});
+    thirdChoice.addEventListener('click', function(){alert('Correct!'), contQuiz()});
+    fourthChoice.addEventListener('click', function(){alert('Incorrect')});
 }
 
 var pressStart = function(){
-  startButton.addEventListener("click", function() {
+  startBtn.addEventListener("click", function() {
     quizBox.style.visibility = "visible";
     function startTimer () {
       var timerInterval = setInterval(function() {
