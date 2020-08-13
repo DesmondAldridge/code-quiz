0a0b3bc052919c4a11ef1d1a16dc3003f9ce1161
Desmond <desmondaldridge@gmail.com>
2020-08-12 18:15:11 -0700

    Finished the code... but doesn't work... tried to debug...

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-// GLOBAL VARIABLES
+// GLOBAL VARIABLES //
 
+// TIMER & START BUTTON
 var theTimer = document.getElementById("timer");
 var theTime = document.getElementById("time");
 var theStart = document.getElementById("startDiv");
 var startButton = document.getElementById("startBtn");
 
-//
-//
+var secondsLeft = 60;
 
-var quizBox = document.getElementById("quizDiv").style.visibility = "hidden";
-
-//
-
+// QUIZ AREA
+var quizBox = document.getElementById("quizDiv");
 var questions = document.getElementById("questionLine");
 var firstChoice = document.getElementById("choice1");
 var secondChoice = document.getElementById("choice2");
 var thirdChoice = document.getElementById("choice3");
 var fourthChoice = document.getElementById("choice4");
 
-//
-//
+//TIMER
+function startTimer () {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    time.textContent = "Seconds left: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      alert('Times Up');
+      toEndPage();
+    }
+  }, 1000);
+}
+
+
+// KEEPING SCORE
+var counter = document.querySelector("#counter");
+var count = localStorage.getItem("count");
+
+// counter.textContent = count;
+
+function addPt() {
+  count++;
+  counter.textContent = count;
+
+  localStorage.setItem("count", count);
+};
+
+// ENDING THE GAME
+
+function toEndPage() {
+  window.location.replace("endPage.html");
+}
 
 // QUESTIONS
-
 function beginQuiz() {
   //First question...
   questions.textcontent = "How many ___ are in ___?";
@@ -39,14 +67,14 @@ function beginQuiz() {
 
 function contQuiz () {
   //Second question...
-  questions.textcontent = "which ___ are ___?";
+  questions.textcontent = "Which ___ are ___?";
   firstChoice.textcontent = "a__";
   secondChoice.textcontent = "b__";
   thirdChoice.textcontent = "c__";
   fourthChoice.textcontent = "d__";
   //Make them choosable
     firstChoice.addEventListener('click', function(){alert('Incorrect')});
-    secondChoice.addEventListener('click', function(){alert('Correct!'), contQuiz2()});
+    secondChoice.addEventListener('click', function(){alert('Correct!'), addPt(), contQuiz2()});
     thirdChoice.addEventListener('click', function(){alert('Incorrect')});
     fourthChoice.addEventListener('click', function(){alert('Incorrect')});
 }
@@ -62,7 +90,7 @@ function contQuiz2 () {
     firstChoice.addEventListener('click', function(){alert('Incorrect')});
     secondChoice.addEventListener('click', function(){alert('Incorrect')});
     thirdChoice.addEventListener('click', function(){alert('Incorrect')});
-    fourthChoice.addEventListener('click', function(){alert('Correct!'), contQuiz3()});
+    fourthChoice.addEventListener('click', function(){alert('Correct!'), addPt(), contQuiz3()});
 }
 
 function contQuiz3 () {
@@ -76,48 +104,13 @@ function contQuiz3 () {
     firstChoice.addEventListener('click', function(){alert('Incorrect')});
     secondChoice.addEventListener('click', function(){alert('Incorrect')});
     thirdChoice.addEventListener('click', function(){alert('Incorrect')});
-    fourthChoice.addEventListener('click', function(){alert('Correct!'), endPage()});
+    fourthChoice.addEventListener('click', function(){alert('Correct!'), addPt(), toEndPage()});
 }
 
 
-
-
-// ONTO THE QUIZ...
-
-//
-//
-
-var secondsLeft = 60;
-
-function beginQuiz() {
-  //First question...
-  questions.textcontent = "How many ___ are in ___?";
-  firstChoice.textcontent = "a__";
-  secondChoice.textcontent = "b__";
-  thirdChoice.textcontent = "c__";
-  fourthChoice.textcontent = "d__";
-  //Make them choosable
-    firstChoice.addEventListener('click', function(){alert('Incorrect')});
-    secondChoice.addEventListener('click', function(){alert('Incorrect')});
-    thirdChoice.addEventListener('click', function(){alert('Correct!'), contQuiz()});
-    fourthChoice.addEventListener('click', function(){alert('Incorrect')});
-}
+// LAUNCHING THE QUIZ...
 
 var pressStart = function(){
-  startBtn.addEventListener("click", function() {
-    quizBox.style.visibility = "visible";
-    function startTimer () {
-      var timerInterval = setInterval(function() {
-        secondsLeft--;
-        time.textContent = secondsLeft;
-    
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          alert('Times Up');
-        }
-    
-      }, 1000);
-    }
-    theStart.style.visibility = "hidden";
-    beginQuiz();
-  })
+  startBtn.addEventListener("click", beginQuiz(), startTimer());
+}
+
